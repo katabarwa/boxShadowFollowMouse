cb14db9328c306825feca8ac5878397b1af953eb
Jesse Katabarwa <mail@jessekatabarwa.info>
2021-02-03 10:06:33 -0500

    click works but not onmousemove

--- public/app.js
+++ public/app.js
@@ -2,14 +2,14 @@ let body = document.querySelector("body");
 body.style.backgroundColor = "black";
 body.style.display = "flex";
 body.style.flexWrap = "wrap";
-document.getElementById("body").addEventListener("click", draw());
-function draw() {
-  // console.log(e);
-  // let x = e.clientX;
-  // let y = e.clientY;
+document.getElementById("body").addEventListener("onmousemove", draw);
+function draw(e) {
+  console.log(e);
+  let x = e.clientX;
+  let y = e.clientY;
 
   // document.getElementById("shape").innerHTML = 80;
-  let numFrame = 10;
+  let numFrame = 2;
   for (let i = 0; i < numFrame; i++) {
     let frame = document.createElement("div");
     // frame.setAttribute("class", "width");
@@ -19,6 +19,9 @@ function draw() {
     // elements[i].style.height = "5px";
     let randomNumber = Math.floor(Math.random() * 2);
     if (randomNumber === 0) {
+      elements[i].style.position = "absolute";
+      elements[i].style.left = `${x}px`;
+      elements[i].style.top = `${y}px`;
       elements[i].style.backgroundColor = "black";
       elements[i].style.width = `${Math.floor(Math.random() * (200 - 50)) +
         50}px`;
@@ -29,6 +32,9 @@ function draw() {
       elements[i].style.boxShadow = "0px 0px 5px 1px #FFFFFF";
     }
     if (randomNumber === 1) {
+      elements[i].style.position = "absolute";
+      elements[i].style.left = `${x}px`;
+      elements[i].style.top = `${y}px`;
       elements[i].style.backgroundColor = "white";
       elements[i].style.width = `${Math.floor(Math.random() * (200 - 50)) +
         50}px`;
@@ -53,9 +59,9 @@ function draw() {
   }
 }
 
-setInterval(() => {
-  draw();
-}, 400);
+// setInterval(() => {
+//   draw();
+// }, 400);
 
 // setTimeout(() => {
 //   draw();
