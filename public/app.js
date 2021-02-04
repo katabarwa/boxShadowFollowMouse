2bc26019acaacc95bbee95bff79575a3dfb2fd80
Jesse Katabarwa <mail@jessekatabarwa.info>
2021-02-04 12:39:22 -0500

    tool working, lets finesse the styling

--- public/app.js
+++ public/app.js
@@ -1,8 +1,14 @@
 let body = document.querySelector("body");
 body.style.backgroundColor = "black";
+let divCanvas = document.getElementById("canvas");
+divCanvas.style.backgroundColor = "black";
+divCanvas.style.height = "2560px";
+divCanvas.style.width = "1600px";
 // body.style.display = "flex";
 // body.style.flexWrap = "wrap";
-document.getElementById("body").addEventListener("onmousemove", clickBody);
+body.addEventListener("mousemove", clickBody);
+// document.getElementById("canvas").addEventListener("onmouseup", clickBody);
+
 function clickBody(e) {
   let x = e.clientX;
   let y = e.clientY;
@@ -11,35 +17,78 @@ function clickBody(e) {
   document.body.appendChild(frame);
   let elements = document.getElementsByClassName("frame");
   console.log("elements-array", elements);
-  for (let i = 0; i < elements.length - 1; i++) {
-    if (i === 0) {
-      elements[elements.length - 1].style.position = "absolute";
-      elements[elements.length - 1].style.left = `${x}px`;
-      elements[elements.length - 1].style.top = `${y}px`;
-      elements[elements.length - 1].style.backgroundColor = "black";
-      elements[elements.length - 1].style.width = `${Math.floor(
-        Math.random() * (200 - 50)
-      ) + 50}px`;
-      elements[elements.length - 1].style.height = `${Math.floor(
-        Math.random() * (200 - 50)
-      ) + 50}px`;
-      elements[elements.length - 1].style.margin = "1px";
-      elements[elements.length - 1].style.borderRadius = "200px";
-      elements[elements.length - 1].style.boxShadow = "0px 0px 5px 1px #FFFFFF";
-    } else {
-      elements[elements.length].style.position = "absolute";
-      elements[elements.length].style.left = `${x}px`;
-      elements[elements.length].style.top = `${y}px`;
-      elements[elements.length].style.backgroundColor = "black";
-      elements[elements.length].style.width = `${Math.floor(
-        Math.random() * (200 - 50)
-      ) + 50}px`;
-      elements[elements.length].style.height = `${Math.floor(
-        Math.random() * (200 - 50)
-      ) + 50}px`;
-      elements[elements.length].style.margin = "1px";
-      elements[elements.length].style.borderRadius = "200px";
-      elements[elements.length].style.boxShadow = "0px 0px 5px 1px #FFFFFF";
+  console.log("elements lenght", elements.length);
+
+  let randomNumber = Math.floor(Math.random() * 2);
+
+  if (randomNumber === 0) {
+    for (let i = 0; i < elements.length; i++) {
+      if (i === 0) {
+        elements[elements.length - 1].style.position = "absolute";
+
+        elements[elements.length - 1].style.backgroundColor = "black";
+        let randomHeight = Math.floor(Math.random() * (200 - 50)) + 50;
+        let randomWidth = Math.floor(Math.random() * (200 - 50)) + 50;
+        elements[elements.length - 1].style.width = `${randomWidth}px`;
+        elements[elements.length - 1].style.height = `${randomHeight}px`;
+        elements[elements.length - 1].style.left = `${x - randomWidth / 2}px`;
+        elements[elements.length - 1].style.top = `${y - randomHeight / 2}px`;
+        elements[elements.length - 1].style.margin = "1px";
+        elements[elements.length - 1].style.borderRadius = "200px";
+        elements[elements.length - 1].style.boxShadow =
+          "0px 0px 5px 1px #FFFFFF";
+      }
+      if (i > 0) {
+        elements[i].style.position = "absolute";
+        let randomHeight = Math.floor(Math.random() * (200 - 50)) + 50;
+        let randomWidth = Math.floor(Math.random() * (200 - 50)) + 50;
+        elements[elements.length - i].style.left = `${x - randomWidth / 2}px`;
+        elements[elements.length - i].style.top = `${y - randomHeight / 2}px`;
+        elements[elements.length - i].style.backgroundColor = "black";
+        elements[elements.length - i].style.width = `${randomWidth}px`;
+        elements[elements.length - i].style.height = `${randomHeight}px`;
+        elements[elements.length - i].style.margin = "1px";
+        elements[elements.length - i].style.borderRadius = "200px";
+        elements[elements.length - i].style.boxShadow =
+          "0px 0px 5px 1px #FFFFFF";
+        return;
+      }
+    }
+  }
+  if (randomNumber === 1) {
+    {
+      for (let i = 0; i < elements.length; i++) {
+        if (i === 0) {
+          elements[elements.length - 1].style.position = "absolute";
+
+          elements[elements.length - 1].style.backgroundColor = "white";
+          let randomHeight = Math.floor(Math.random() * (200 - 50)) + 50;
+          let randomWidth = Math.floor(Math.random() * (200 - 50)) + 50;
+          elements[elements.length - 1].style.width = `${randomWidth}px`;
+          elements[elements.length - 1].style.height = `${randomHeight}px`;
+          elements[elements.length - 1].style.left = `${x - randomWidth / 2}px`;
+          elements[elements.length - 1].style.top = `${y - randomHeight / 2}px`;
+          elements[elements.length - 1].style.margin = "1px";
+          elements[elements.length - 1].style.borderRadius = "200px";
+          elements[elements.length - 1].style.boxShadow =
+            "0px 0px 5px 1px #FFFFFF";
+        }
+        if (i > 0) {
+          elements[i].style.position = "absolute";
+          let randomHeight = Math.floor(Math.random() * (200 - 50)) + 50;
+          let randomWidth = Math.floor(Math.random() * (200 - 50)) + 50;
+          elements[elements.length - i].style.left = `${x - randomWidth / 2}px`;
+          elements[elements.length - i].style.top = `${y - randomHeight / 2}px`;
+          elements[elements.length - i].style.backgroundColor = "white";
+          elements[elements.length - i].style.width = `${randomWidth}px`;
+          elements[elements.length - i].style.height = `${randomHeight}px`;
+          elements[elements.length - i].style.margin = "1px";
+          elements[elements.length - i].style.borderRadius = "200px";
+          elements[elements.length - i].style.boxShadow =
+            "0px 0px 5px 1px #FFFFFF";
+          return;
+        }
+      }
     }
   }
 }
