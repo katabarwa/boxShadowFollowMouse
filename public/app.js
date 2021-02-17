9ca0392fd300727d3f2bdedd4c9e8a89c4ac5822
Jesse Katabarwa <mail@jessekatabarwa.info>
2021-02-17 18:20:52 -0500

    tool working switching svg file

--- public/app.js
+++ public/app.js
@@ -1,7 +1,8 @@
 class DirectionAwareShadow {
-  constructor({ target = null, def = null } = {}) {
+  constructor({ target = null, def = null, def2 = null } = {}) {
     this.target = target ? document.querySelectorAll(target) : target;
     this.def = def ? document.querySelectorAll(def) : def;
+    this.def2 = def2 ? document.querySelectorAll(def2) : def2;
   }
 
   move() {
@@ -14,8 +15,8 @@ class DirectionAwareShadow {
     const { currentTarget, offsetX, offsetY } = e;
     const { offsetHeight, offsetWidth } = currentTarget;
 
-    const x = ((offsetWidth / 2 - offsetX) * 0.8) / 2 / 10;
-    const y = ((offsetHeight / 2 - offsetY) * 0.8) / 2 / 10;
+    const x = ((offsetWidth / 2 - offsetX) * 0.8) / 2 / 30;
+    const y = ((offsetHeight / 2 - offsetY) * 0.8) / 2 / 30;
 
     this.def[0].innerHTML = `<filter id="f4" x="-2px" y="-2px" width="500%" height="500%">
     <feOffset id="f4bub" result="offOut" in="SourceGraphic" dx="${x}" dy="${y}"></feOffset>
@@ -23,6 +24,53 @@ class DirectionAwareShadow {
     <feGaussianBlur result="blurOut" in="matrixOut" stdDeviation="10"></feGaussianBlur>
     <feBlend in="SourceGraphic" in2="blurOut" mode="normal"></feBlend>
   </filter>`;
+    this.def2[0].innerHTML = `<filter
+  id="innershadow"
+  x0="-50%"
+  y0="-50%"
+  width="500%"
+  height="500%"
+>
+  <feGaussianBlur
+    in="SourceAlpha"
+    stdDeviation="3"
+    result="blur"
+  ></feGaussianBlur>
+  <feOffset id="offSet" dy="${y}" dx="${x}"></feOffset>
+  <feComposite
+    in2="SourceAlpha"
+    operator="arithmetic"
+    k2="-1"
+    k3="1"
+    result="shadowDiff"
+  ></feComposite>
+
+  <feFlood flood-color="#444444" flood-opacity="0.75"></feFlood>
+  <feComposite in2="shadowDiff" operator="in"></feComposite>
+  <feComposite
+    in2="SourceGraphic"
+    operator="over"
+    result="firstfilter"
+  ></feComposite>
+
+  <feGaussianBlur
+    in="firstfilter"
+    stdDeviation="3"
+    result="blur2"
+  ></feGaussianBlur>
+  <feOffset id="offSet" dy="${y}" dx="${x}"></feOffset>
+  <feComposite
+    in2="firstfilter"
+    operator="arithmetic"
+    k2="-1"
+    k3="1"
+    result="shadowDiff"
+  ></feComposite>
+
+  <feFlood flood-color="#444444" flood-opacity="0.75"></feFlood>
+  <feComposite in2="shadowDiff" operator="in"></feComposite>
+  <feComposite in2="firstfilter" operator="over"></feComposite>
+</filter>`;
   }
 
   init() {
@@ -40,7 +88,8 @@ const bub = new DirectionAwareShadow({
   svg: "#mark",
   filter: "#f4",
   feOffset: "#f4bub",
-  def: "#def"
+  def: "#def",
+  def2: "#deftwo"
 });
 
 bub.init();
