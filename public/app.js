84037cf4a82c6c1841efce5b0c8768122bd95383
Jesse Katabarwa <mail@jessekatabarwa.info>
2021-02-18 10:00:26 -0500

    styling okay, both inner shadow works

--- public/app.js
+++ public/app.js
@@ -27,100 +27,100 @@ class DirectionAwareShadow {
     //   <feGaussianBlur result="blurOut" in="matrixOut" stdDeviation="10"></feGaussianBlur>
     //   <feBlend in="SourceGraphic" in2="blurOut" mode="normal"></feBlend>
     // </filter>`;
-    //     this.def2[0].innerHTML = `<filter
-    //   id="innershadow"
-    //   x0="-50%"
-    //   y0="-50%"
-    //   width="500%"
-    //   height="500%"
-    // >
-    //   <feGaussianBlur
-    //     in="SourceAlpha"
-    //     stdDeviation="3"
-    //     result="blur"
-    //   ></feGaussianBlur>
-    //   <feOffset id="offSet" dy="${y}" dx="${x}"></feOffset>
-    //   <feComposite
-    //     in2="SourceAlpha"
-    //     operator="arithmetic"
-    //     k2="-1"
-    //     k3="1"
-    //     result="shadowDiff"
-    //   ></feComposite>
+    this.def2[0].innerHTML = `<filter
+      id="innershadow"
+      x0="-50%"
+      y0="-50%"
+      width="500%"
+      height="500%"
+    >
+      <feGaussianBlur
+        in="SourceAlpha"
+        stdDeviation="3"
+        result="blur"
+      ></feGaussianBlur>
+      <feOffset id="offSet" dy="${y2}" dx="${x2}"></feOffset>
+      <feComposite
+        in2="SourceAlpha"
+        operator="arithmetic"
+        k2="-1"
+        k3="1"
+        result="shadowDiff"
+      ></feComposite>
 
-    //   <feFlood flood-color="white" flood-opacity="0.75"></feFlood>
-    //   <feComposite in2="shadowDiff" operator="in"></feComposite>
-    //   <feComposite
-    //     in2="SourceGraphic"
-    //     operator="over"
-    //     result="firstfilter"
-    //   ></feComposite>
+      <feFlood flood-color="black" flood-opacity="0.1"></feFlood>
+      <feComposite in2="shadowDiff" operator="in"></feComposite>
+      <feComposite
+        in2="SourceGraphic"
+        operator="over"
+        result="firstfilter"
+      ></feComposite>
 
-    //   <feGaussianBlur
-    //     in="firstfilter"
-    //     stdDeviation="3"
-    //     result="blur2"
-    //   ></feGaussianBlur>
-    //   <feOffset id="offSet" dy="${y}" dx="${x}"></feOffset>
-    //   <feComposite
-    //     in2="firstfilter"
-    //     operator="arithmetic"
-    //     k2="-1"
-    //     k3="1"
-    //     result="shadowDiff"
-    //   ></feComposite>
+      <feGaussianBlur
+        in="firstfilter"
+        stdDeviation="3"
+        result="blur2"
+      ></feGaussianBlur>
+      <feOffset id="offSet" dy="${y2}" dx="${x2}"></feOffset>
+      <feComposite
+        in2="firstfilter"
+        operator="arithmetic"
+        k2="-1"
+        k3="1"
+        result="shadowDiff"
+      ></feComposite>
 
-    //   <feFlood flood-color="white" flood-opacity="0.75"></feFlood>
-    //   <feComposite in2="shadowDiff" operator="in"></feComposite>
-    //   <feComposite in2="firstfilter" operator="over"></feComposite>
-    // </filter>`;
+      <feFlood flood-color="black" flood-opacity="0.1"></feFlood>
+      <feComposite in2="shadowDiff" operator="in"></feComposite>
+      <feComposite in2="firstfilter" operator="over"></feComposite>
+    </filter>`;
     this.def3[0].innerHTML = `<filter
-    id="innershadowTwo"
-    x="-50%"
-    y="-50%"
-    width="200%"
-    height="200%"
-  >
-    <feGaussianBlur
-      in="SourceAlpha"
-      stdDeviation="1"
-      result="blur"
-    ></feGaussianBlur>
-    <feOffset dy="${y2}" dx="${x2}"></feOffset>
-    <feComposite
-      in2="SourceAlpha"
-      operator="arithmetic"
-      k2="-1"
-      k3="1"
-      result="shadowDiff"
-    ></feComposite>
+      id="innershadowTwo"
+      x="-50%"
+      y="-50%"
+      width="200%"
+      height="200%"
+    >
+      <feGaussianBlur
+        in="SourceAlpha"
+        stdDeviation="1"
+        result="blur"
+      ></feGaussianBlur>
+      <feOffset dy="${-y2}" dx="${-x2}"></feOffset>
+      <feComposite
+        in2="SourceAlpha"
+        operator="arithmetic"
+        k2="-1"
+        k3="1"
+        result="shadowDiff"
+      ></feComposite>
 
-    <feFlood flood-color="black" flood-opacity="0.25"></feFlood>
-    <feComposite in2="shadowDiff" operator="in"></feComposite>
-    <feComposite
-      in2="SourceGraphic"
-      operator="over"
-      result="firstfilter"
-    ></feComposite>
+      <feFlood flood-color="black" flood-opacity="0.25"></feFlood>
+      <feComposite in2="shadowDiff" operator="in"></feComposite>
+      <feComposite
+        in2="SourceGraphic"
+        operator="over"
+        result="firstfilter"
+      ></feComposite>
 
-    <feGaussianBlur
-      in="firstfilter"
-      stdDeviation="3"
-      result="blur2"
-    ></feGaussianBlur>
-    <feOffset dy="${y2}" dx="${x2}"></feOffset>
-    <feComposite
-      in2="firstfilter"
-      operator="arithmetic"
-      k2="-1"
-      k3="-9"
-      result="shadowDiff"
-    ></feComposite>
+      <feGaussianBlur
+        in="firstfilter"
+        stdDeviation="3"
+        result="blur2"
+      ></feGaussianBlur>
+      <feOffset dy="${-y2}" dx="${-x2}"></feOffset>
+      <feComposite
+        in2="firstfilter"
+        operator="arithmetic"
+        k2="-1"
+        k3="-9"
+        result="shadowDiff"
+      ></feComposite>
 
-    <feFlood flood-color="black" flood-opacity="0.25"></feFlood>
-    <feComposite in2="shadowDiff" operator="in"></feComposite>
-    <feComposite in2="firstfilter" operator="over"></feComposite>
-  </filter>`;
+      <feFlood flood-color="black" flood-opacity="0.25"></feFlood>
+      <feComposite in2="shadowDiff" operator="in"></feComposite>
+      <feComposite in2="firstfilter" operator="over"></feComposite>
+    </filter>`;
   }
 
   init() {
